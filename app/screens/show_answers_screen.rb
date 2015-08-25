f92beb4703829e196450d8eabc00051b611b0649
name": null <email": null>
2015-08-25 10:55:51 -0700

    added label

--- app/screens/show_answers_screen.rb
+++ app/screens/show_answers_screen.rb
@@ -3,15 +3,28 @@ class ShowAnswersScreen < PM::Screen
   attr_accessor :question
 
   def on_load
-    self.title = answer.title
+    self.title = "Antworten"
 
-    @layout = QuestionAnswerLayout.new(root: self.view)
-    @layout.build
-    @layout.get(:notes).text = question.notes
+    # @layout = QuestionAnswerLayout.new(root: self.view)
+    # @layout.build
+    # @layout.get(:notes).text = question.notes
+    set_nav_bar_button :right, title: "Frage", action: :close_answer
   end
 
-  def updateViewConstraints
-    @layout.add_top_layout_guide_constraint(self)
-    super
+  # def content
+  # end
+
+  # def load_failed
+  # end
+
+  # def updateViewConstraints
+  #   @layout.add_top_layout_guide_constraint(self)
+  #   super
+  # end
+
+  def close_answer#(question)
+    close
+    # open ShowQuestionsScreen.new(nav_bar: true)
+    # open ShowAnswersScreen.new({ answer: question })
   end
 end
--- app/screens/show_questions_screen.rb
+++ app/screens/show_questions_screen.rb
@@ -1,25 +1,42 @@
 
-class ShowQuestionsScreen < PM::TableScreen #this needs to change
-  title "Fragen"
+class ShowQuestionsScreen < PM::Screen #this needs to change
+  # refreshable callback: :on_refresh,
+  #             pull_message: "Pull to refresh",
+  #             refreshing: "Refreshing data...",
+  #             updated_format: "Last updated at %s",
+  #             updated_time_format: "%l:%M %p"
 
-  def table_data
-    [
-      {
-        #all this needs to change
-        cells: Question.all.map do |t|
-          {
-            title: t.title,
-            subtitle: t.notes,
-            cell_style: UITableViewCellStyleSubtitle,
-            action: :show_answer,
-            arguments: t
-          }
-        end
-      }
-    ]
+
+
+  def on_load
+    self.title = "Fragen"
+    set_nav_bar_button :left, title:"Antwort", action: :show_answer
+    #change to tab bar
   end
 
-  def show_answer(question)
-    open ShowAnswersScreen.new({ answer: question })
+  def will_appear
+    set_attributes self.view, {
+      background_color: hex_color("#FFFFFF") #323232 dark grey
+    }
+
+    add UILabel.new, {
+      text: "Question phrase goes here?",
+      font: UIFont.systemFontOfSize(32),
+      left: 20,
+      top: 200,
+      width: 280,
+      height: 50,
+      text_alignment: NSTextAlignmentCenter
+    }
+  end
+
+  # def on_refresh
+  #   end_refreshing
+  # end
+
+  def show_answer#(question)
+    open_modal ShowAnswersScreen.new(nav_bar: true)
+    #open_modal ...one option
+    # open ShowAnswersScreen.new({ answer: question })
   end
 end
